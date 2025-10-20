c857efe50ed4db60bcb99ba002f4e8173bfba06c
laura <laurapapa693@gmail.com>
2025-10-20 10:46:21 -0300

    add adiciionar, listar, atualizar,del, pesquisar

--- back end/funcao.py
+++ back end/funcao.py
@@ -23,3 +23,94 @@ def criar_tabela():
 
 
 criar_tabela()
+
+
+#-----------
+
+def adicionar_produto(nome, categoria, preco, quantidade):
+    conexao, cursor = conectar()
+    if conexao:
+        try:
+            cursor.execute(
+                "INSERT INTO produto (nome, categoria, preco, quantidade) VALUES (%s, %s, %s, %s)",
+                (nome, categoria, preco, quantidade)
+            )
+            conexao.comit()
+        except Exception as erro:
+            print(F"Erro ao inserir {erro}")
+        finally:
+            cursor.close()
+            conexao.close()
+
+#-----------
+
+def listar_total():
+    conexao, cursor = conectar()
+    if conexao:
+        try:
+            cursor.execute(
+                "SELECT * FROM produto ORDER BY id"
+            )
+            return cursor.fetchall()
+        except Exception as erro:
+            print(f"Erro ao listar {erro}")
+            return []
+        finally:
+            cursor.close()
+            conexao.close()
+
+produtos = listar_total()
+print(produtos)
+
+#------------
+
+def atualizar_preco_quantidade(preco, quantidade):
+    conexao, cursor = conectar()
+    if conexao:
+        try:
+            cursor.execute(
+                "UPDAT produto SET preco = %s WHERE id = %s",
+                "UPDATE produto SET quantidade = %s WHERE id = %s",
+                (quantidade, preco)
+            )
+            conexao.commit()
+        except Exception as erro:
+            print(f"Erro as atualizar a nota: {erro}")
+        finally:
+            cursor.close()
+            conexao.close()
+
+#------------
+
+def deletar_movie(id_produto):
+    conexao, cursor = conectar()
+    if conexao:
+        try:
+            cursor.execute(
+                "DELETE FROM produto WHERE id = %s, (id_produto,)"
+            )
+            conexao.comit()
+        except Exception as erro:
+            print(f"Erro ao deletar filme: {erro}")
+        finally:
+            cursor.close()
+            conexao.close()
+
+#-------------
+
+def pesquisar_produto(nome_parcial):
+    conexao, cursor = conectar()
+    if conexao:
+        try:
+            cursor.execute(
+                "SELECT * FROM produto WHERE nome ILIKE %s ORDER BY id",
+                (f"%{nome_parcial}%", )
+            )
+            resultados = cursor.fetchall()
+            return resultados
+        except Exception as erro:
+            print(f"Erro ao pesquisar produtp: {erro}")
+            return []
+        finally:
+            cursor.close()
+            conexao.close()
